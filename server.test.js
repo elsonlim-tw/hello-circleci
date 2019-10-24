bd2dbaf18a6ef34a74f4278c9a1bbf9d846ca5a7
elsonlim <elsonlim7@gmail.com>
2019-10-24 15:49:32 +0800

    hello world

---
+++ server.test.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("./app");
+
+describe("Server", () => {
+  it("/ GET should return Hello World", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.text).toBe("Hello World!");
+  });
+});
